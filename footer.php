288306deace46213b90565c71b7fbb5cd2b34a75
eguaman1990 <es.aguaman@gmail.com>
2015-07-10 17:39:11 -0300

    Merge origin/master
    Conflicts:
    	about.html

---
+++ footer.php
@@ -0,0 +1,15 @@
+<!-- Footer -->
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p class="pull-right">
+                Milano  - Tomas Andrews 081, Providencia - Teléfono: (56 2) 24892900 Fax: (56 2) 6359763
+            </p>
+            <p>
+                Copyright &copy; Eguaman 2015
+                <a href="http://webmail.milano.cl" target="_blank">WebMail</a>
+            </p>
+        </div>
+    </div>
+</footer>
+
---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Edwin Guamán">
+
+        <title>Cobranzas y Servicios Milano</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/modern-business.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <?php include_once 'navbar.php'; ?>
+
+
+
+        <!-- Page Content -->
+        <div class="container">
+
+
+            <!-- Intro Content -->
+            <div class="row">
+                <div class="col-md-8">
+                    <h2 class="title-red">Optimice sus cobranzas con Milano.</h2>
+                    <p>Somos una empresa que nace en Santiago de Chile en el año 1994, orientada a la Cobranza del Area Médica y que actualmente cuenta con un Recurso Humano especializado, infraestructura y tecnología de punta que nos permite ofrecer soluciones de alta calidad a nuestros Clientes tanto de Empresas Públicas como Privadas.</p>
+                    <p><b>Misión:</b> Optimizar la recuperación de las cuentas de nuestros clientes y la búsqueda e implementación de nuevos servicios relacionados, que nos permitan satisfacer nuevas necesidades del mercado objetivo.</p>
+                    <p><b>Visión:</b> Estamos convencidos que la tercerizacion en la recuperación de las carteras en mora, ha pasado a ser un punto de vital importancia para la actual dinámica de los negocios. Por esto, deseamos ser la mejor opción de Outsourcing de Cobranzas y de otros servicios complementarios, para ser un socio estratégico para el cliente.</p>
+                    <h4 class="title-red">Nuestra Ubicación</h4>
+                    <img class="img-responsive" src="images/Ubicacion.PNG" alt="">
+                </div>
+                <div class="col-md-4">
+                    <h4 class="title-red">Nuestros Servicios</h4>
+                    <ul class="nav nav-pills nav-stacked">
+                        <li role="presentation"><a href="#">Home</a></li>
+                        <li role="presentation"><a href="#">Profile</a></li>
+                        <li role="presentation"><a href="#">Messages</a></li>
+                    </ul>
+                    <h4 class="title-red">Filosofía de Trabajo</h4>
+                    <ul class="nav nav-pills nav-stacked">
+                        <li role="presentation"><a href="#">Home</a></li>
+                        <li role="presentation"><a href="#">Profile</a></li>
+                        <li role="presentation"><a href="#">Messages</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /.row -->
+
+
+
+            <?php include_once './footer.php'; ?>
+
+        </div>
+        <!-- /.container -->
+
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+
+</html>
